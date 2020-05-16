60637f0d43d679c6ff5434c43ed6f22b7e705d8f
Khuong Tran <dbcgdhh@gmail.com>
2020-05-16 10:35:08 +0700

    Init plugin

---
+++ vam-mentortee.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Plugin Name: Vam Mentortee
+ * Plugin URI: 
+ * Description: Managing plugin for mentor and mentee
+ * Version: 1.0
+ * Author: TDAK
+ * Author URI: http://tdak.me
+ */
