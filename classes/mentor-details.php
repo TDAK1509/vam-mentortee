b617ebc3d841549027a6ea3cfb7f0d681d95225a
Khuong Tran <dbcgdhh@gmail.com>
2020-07-26 13:13:31 +0700

    Get user data by id mentor details

--- classes/mentor-details.php
+++ classes/mentor-details.php
@@ -11,6 +11,13 @@ class VamMentorDetails {
         $this->userId = $this->getUserId();
     }
 
+    private function getUserId() {
+        $regex = "/mentor-details\/([0-9]+)\//";
+        $pageUri = $_SERVER['REQUEST_URI'];
+        preg_match($regex, $pageUri, $matches);
+        return intval($matches[1]);
+    }
+
     public static function init() {
         $self = new self();
         add_action('wp_enqueue_scripts', [$self, 'importCss']);
@@ -22,8 +29,8 @@ class VamMentorDetails {
     }
 
     public function getTemplate() {
-        $userId = $this->userId;
-        echo $userId . "<br>";
+        $userInfo = $this->getMentorInfo();
+        print_r($userInfo);
 
         $html = "
         <div class='mentor-details'>
@@ -34,10 +41,42 @@ class VamMentorDetails {
         return $html;
     }
 
-    private function getUserId() {
-        $regex = "/mentor-details\/([0-9]+)\//";
-        $pageUri = $_SERVER['REQUEST_URI'];
-        preg_match($regex, $pageUri, $matches);
-        return $matches[1];
+    private function getMentorInfo() {
+        $mentors = $this->getMentors();
+
+        foreach($mentors as $mentor) {
+            if ($mentor->ID === $this->userId) {
+                $userMetaData = get_user_meta($mentor->ID);
+
+                return [
+                    "id" => $mentor->ID,
+                    "avatar" => get_avatar_url($mentor->user_email),
+                    "display_name" => $mentor->display_name,
+                    "company" => $userMetaData["company"][0],
+                    "title" => $userMetaData["title"][0],
+                    "email" => $mentor->user_email,
+                    "phone" => $userMetaData["phone"][0],
+                    "biography" => $userMetaData["biography"][0],
+                    "year_of_experience" => $userMetaData["year_of_experience"][0],
+                    "degree" => $userMetaData["degree"][0],
+                    "career_field" => $userMetaData["career_field"][0],
+                    "expertise" => $userMetaData["expertise"][0],
+                    "topics" => $userMetaData["topics"][0],
+                    "hobbies" => $userMetaData["hobbies"][0],
+                    "mentoring_program" => $userMetaData["mentoring_program"][0],
+                ];
+            }
+        }
+
+        echo "cannot find user";
+        return [];
+    }
+
+    private function getMentors() {
+        return get_users([
+            'role' => 'mentor',
+            'orderby' => 'user_registered',
+            'order' => 'ASC'
+        ]);
     }
 }
