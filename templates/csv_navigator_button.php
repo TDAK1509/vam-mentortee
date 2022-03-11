3877360ae4a247e0d97b4186359b29cb7a147ba7
Khuong Tran <dbcgdhh@gmail.com>
2022-03-11 12:30:18 +0700

    Read csv to array

--- templates/csv_navigator_button.php
+++ templates/csv_navigator_button.php
@@ -1,1 +1,17 @@
-<h1>TEST</h1>
+<h1>TẠO MENTOR BẰNG FILE CSV</h1>
+
+<form method="POST" action="<?php echo admin_url( 'admin.php' ) . '?page=upload_mentor_csv'; ?>" enctype="multipart/form-data">
+  <input type="file" name="mentor_csv" />
+  <button class="mentor-csv__button">Tải</button>
+</form>
+
+<?php
+if (isset($_FILES)) {
+  $tmpName = $_FILES['mentor_csv']['tmp_name'];
+
+  if ($tmpName == "") return;
+
+  $csvAsArray = array_map('str_getcsv', file($tmpName));
+  var_dump($csvAsArray);
+}
+?>
